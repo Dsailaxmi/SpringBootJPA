afa3fddc560757515d6bf11f406fbe0798cd21a7
Dsailaxmi <98604399+Dsailaxmi@users.noreply.github.com>
2022-02-28 23:08:36 +0530

    SpringBootJPA

---
+++ SpringHibernateLogic.java
@@ -0,0 +1,36 @@
+package com.mondee;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringHibernateLogic implements CommandLineRunner{
+	
+	@Autowired
+	private EmployeeDao dao;
+	public static void main(String[] args) {
+	ConfigurableApplicationContext ca=SpringApplication.run(SpringHibernateLogic.class, args);
+	
+	
+	}
+
+	public void run(String... args) throws Exception {
+		Employee emp=new Employee();
+		emp.setId(100);
+		emp.setName("lucky");
+		emp.setSalary(90000);
+		dao.insert(emp);
+		
+		System.out.println("\n\t Employee Data \n");
+		dao.select();
+
+		dao.delete(2);
+
+		dao.select();
+
+	}
+	 
+}
